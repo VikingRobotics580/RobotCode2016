58fc1132ed7e7c608fcb805f2cd8f98857c866f6
Tyler Robbins <tyler@familyrobbins.com>
2015-12-09 12:02:12 -0800

    Added blank Robot class.

---
+++ src/Robot.cpp
@@ -0,0 +1,47 @@
+/*
+ * Robot Vikings
+ * Robot.cpp
+ * 12/6/15
+ */
+
+#include "WPILib.h"
+#include "Robot.h"
+
+Robot::Robot(){
+
+}
+
+Robot::~Robot(){
+
+}
+
+void Robot::RobotInit(){
+
+}
+void Robot::AutonomousInit(){
+
+}
+void Robot::TeleopInit(){
+
+}
+void Robot::TestInit(){
+
+}
+void Robot::DisabledInit(){
+
+}
+
+void Robot::AutonomousPeriodic(){
+
+}
+void Robot::TeleopPeriodic(){
+
+}
+void Robot::TestPeriodic(){
+
+}
+void Robot::DisabledPeriodic(){
+
+}
+
+START_ROBOT_CLASS(Robot);
---
+++ src/Robot.h
@@ -0,0 +1,94 @@
+/*
+ * RobotVikings
+ * Robot.h
+ * 12/6/15
+ * Declares a class Robot as the main class.
+ */
+
+#ifndef ROBOT_H_
+#define ROBOT_H_
+
+class IterativeRobot;
+
+class Robot: public IterativeRobot{
+    public:
+        Robot();
+        ~Robot();
+
+    protected:
+        /*
+         * RobotInit
+         * Accepts nothing
+         * Returns nothing
+         * Initializes the Robot upon startup
+         */
+        void RobotInit() override;
+
+        /*
+         * AutonomousInit
+         * Accepts nothing
+         * Returns nothing
+         * Prepares the robot for Autonomous mode
+         */
+        void AutonomousInit() override;
+
+        /*
+         * TeleopInit
+         * Accepts nothing
+         * Returns nothing
+         * Prepares the robot for Teleop mode
+         */
+        void TeleopInit() override;
+
+        /*
+         * TestInit
+         * Accepts nothing
+         * Returns nothing
+         * Prepares the robot for Test mode
+         */
+        void TestInit() override;
+
+        /*
+         * DisabledInit
+         * Accepts nothing
+         * Returns nothing
+         * Prepares the robot for Disabled mode
+         */
+        void DisabledInit() override;
+
+        /*
+         * AutonomousPeriodic
+         * Accepts nothing
+         * Returns nothing
+         * Is run periodically during Autonomous mode
+         */
+        void AutonomousPeriodic() override;
+
+        /*
+         * TeleopPeriodic
+         * Accepts nothing
+         * Returns nothing
+         * Is run periodically during Teleop mode
+         */
+        void TeleopPeriodic() override;
+
+        /*
+         * TestPeriodic
+         * Accepts nothing
+         * Returns nothing
+         * Is run periodically during Test mode
+         */
+        void TestPeriodic() override;
+
+        /*
+         * DisabledPeriodic
+         * Accepts nothing
+         * Returns nothing
+         * Is run periodically during Disabled mode
+         */
+        void DisabledPeriodic() override;
+};
+
+
+
+#endif
