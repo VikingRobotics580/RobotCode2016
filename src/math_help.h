393a084c7925faa4d5c16905564dc84ed386e9c7
Tyler Robbins <tyler@familyrobbins.com>
2015-12-09 19:49:29 -0800

    Added files that define and declare math functions for help with math-related tasks.

---
+++ src/math_help.h
@@ -0,0 +1,22 @@
+/*
+ * RobotVikings
+ * math.h
+ * 12/6/15
+ * Declares math functions to help with whatever we may need them for.
+ */
+
+#ifndef _MATH_HELP_H_
+#define _MATH_HELP_H_
+
+namespace math_help{
+ /*
+  * Q_rsqrt
+  * Accepts a float
+  * Returns a float
+  * Quickly and efficiently calculates x^(-1/2), where x is any floating point number.
+  */
+ float Q_rsqrt(float);
+
+}
+
+#endif
