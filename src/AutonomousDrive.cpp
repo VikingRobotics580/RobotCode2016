e68cf09590e8f160b2f70b82569ca3a703e05e1a
Tyler Robbins <tyler@familyrobbins.com>
2016-01-08 08:53:19 -0800

    Added source file for AutonomousDrive class.

---
+++ src/AutonomousDrive.cpp
@@ -0,0 +1,35 @@
+/*
+ * AutonomousDrive.cpp
+ *
+ *  Created on: Dec 14, 2015
+ *      Author: tyler
+ */
+
+AutonomousDrive::AutonomousDrive(){
+}
+
+AutonomousDrive::~AutonomousDrive(){
+}
+
+void AutonomousDrive::Initialize(){
+}
+
+void AutonomousDrive::Execute(){
+}
+
+bool AutonomousDrive::IsFinished(){
+}
+
+void AutonomousDrive::End(){
+}
+
+void AutonomousDrive::Interrupted(){
+}
+
+int AutonomousDrive::mode(){
+}
+
+int AutonomousDrive::readAutoSyntax(){
+}
+
+
