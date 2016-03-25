f567252b239f507d32e1cb313cc2fc7a9d8e5e16
Tyler Robbins <tyler@familyrobbins.com>
2016-03-24 20:46:02 -0700

    Fixed an off-by-one error.

--- src/buttons.h
+++ src/buttons.h
@@ -3,20 +3,20 @@
 #ifndef _BUTTONS_H_
 #define _BUTTONS_H_
 
-#define RAISE_BUTTON         2
-#define LOWER_BUTTON         3
-#define EXTEND_BUTTON        4
-#define RETRACT_BUTTON       5
+#define RAISE_BUTTON         1
+#define LOWER_BUTTON         2
+#define EXTEND_BUTTON        3
+#define RETRACT_BUTTON       4
 #define SUCK_BUTTON          5
-#define LAUNCH_BUTTON        7
-#define WINCH_BUTTON         8
-#define RELEASE_BUTTON       9
-#define MISSILE_BUTTON       10
-#define LAUNCH_INIT_BUTTON   11
+#define LAUNCH_BUTTON        6
+#define WINCH_BUTTON         7
+#define RELEASE_BUTTON       8
+#define MISSILE_BUTTON       9
+#define LAUNCH_INIT_BUTTON   10
 
-#define AUTO_SWITCH_1		 12
-#define AUTO_SWITCH_2		 13
-#define AUTO_SWITCH_3		 1
+#define AUTO_SWITCH_1		 11
+#define AUTO_SWITCH_2		 12
+#define AUTO_SWITCH_3		 0
 
 #endif
 
