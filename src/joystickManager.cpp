4e1088c9f11365de378177539143c868daf1a8c7
Tyler Robbins <tyler@familyrobbins.com>
2016-03-26 15:12:35 -0700

    Reenabled extension and retraction. Switched back to the binary switch way of Autonomous.

--- src/joystickManager.cpp
+++ src/joystickManager.cpp
@@ -95,9 +95,9 @@ int joystickManager::checkTeleopButtons(){
 
     // Do Extend stuff
     if(m_joysticks["buttonBox"]->GetButton(EXTEND_BUTTON)){
-        //ret |= m_hardware_manager->extend();
+        ret |= m_hardware_manager->extend();
     }else if(m_joysticks["buttonBox"]->GetButton(RETRACT_BUTTON)){
-        //ret |= m_hardware_manager->retract();
+        ret |= m_hardware_manager->retract();
     }else{
         ret |= m_hardware_manager->stopExtRet();
     }
@@ -126,16 +126,17 @@ int joystickManager::checkAutoButtons(){
 
     // TODO: I've disabled the binary-switch thing for now, but let us make sure we fix it later okay?
     // Also note, the system that has been put in place temporarily while I figure out the binary thing only allows for 3 modes, not 8, so be careful about that.
-    /*
-    auto_mode |= m_joysticks["buttonBox"]->GetButton(AUTO_SWITCH_1);
+    auto_mode |= m_joysticks["buttonBox"]->GetButton(AUTO_SWITCH_3);
     auto_mode <<= 1;
     auto_mode |= m_joysticks["buttonBox"]->GetButton(AUTO_SWITCH_2);
     auto_mode <<= 1;
-    auto_mode |= m_joysticks["buttonBox"]->GetButton(AUTO_SWITCH_3);
-    */
+    auto_mode |= m_joysticks["buttonBox"]->GetButton(AUTO_SWITCH_1);
+
+    /*
     if(m_joysticks["buttonBox"]->GetButton(AUTO_SWITCH_1)) auto_mode = 1;
     if(m_joysticks["buttonBox"]->GetButton(AUTO_SWITCH_2)) auto_mode = 2;
     if(m_joysticks["buttonBox"]->GetButton(AUTO_SWITCH_3)) auto_mode = 3;
+    */
 
     if(this->m_robot->getAutoMan()->getMode() != auto_mode)
         this->m_robot->getAutoMan()->setMode(auto_mode);
