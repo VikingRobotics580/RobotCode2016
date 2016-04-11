cd34cd1cda4c8f8de932c3a27df3dc2c77d80441
Tyler Robbins <tyler@familyrobbins.com>
2016-04-11 14:34:14 -0700

    Added whitespace.

--- src/buttons.h
+++ src/buttons.h
@@ -7,11 +7,16 @@
 #define LOWER_BUTTON         2
 #define EXTEND_BUTTON        3
 #define RETRACT_BUTTON       4
+
 #define SUCK_BUTTON          5
 #define LAUNCH_BUTTON        6
+
 #define WINCH_BUTTON         7
+
 #define RELEASE_BUTTON       8
+
 #define MISSILE_BUTTON       9
+
 #define LAUNCH_INIT_BUTTON   10
 
 #define AUTO_SWITCH_1		 11
