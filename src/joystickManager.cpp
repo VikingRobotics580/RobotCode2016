d3369cb9aa35e28dc711ddd36bb7e4625442b60b
Tyler Robbins <tyler@familyrobbins.com>
2016-04-12 11:08:00 -0700

    Added comments, and replaced a few comments.

--- src/joystickManager.cpp
+++ src/joystickManager.cpp
@@ -9,6 +9,7 @@ joystickManager::joystickManager(HardwareManager* hw,Robot* robot):
 {
     this->m_hardware_manager = hw;
     this->m_robot = robot;
+    // Register all 3 joysticks we will be using (the ID's are kind of arbitrary, but necessary at the same time)
     m_joysticks["leftHand"] = new joystick(0,1,5,hw);
     m_joysticks["rightHand"] = new joystick(1,1,5,hw);
     m_joysticks["buttonBox"] = new joystick(2,13,1,hw);
@@ -22,30 +23,36 @@ joystickManager::~joystickManager(){
 
 int joystickManager::Init(){
     int ret = 0;
+    // Initialize each joystick we have registered
     for(auto& set : this->m_joysticks){
+        // 1 will be returned if any of the Init() calls return 1 themselves
         ret |= set.second->Init();
     }
     return ret;
 }
 
 int joystickManager::Update(){
+    // Get the current Robot mode, so that we can call the appropriate check*Buttons method
     RModes mode = Robot::s_mode;
     int ret = 0;
     try{
         switch(mode){
             case RModes::TELEOP:
+                // Unlike the other modes, here we check for joystick input and move the wheels
                 ret |= m_hardware_manager->move(m_joysticks["leftHand"]->GetAxis(1),
                         -m_joysticks["rightHand"]->GetAxis(1));
-                //log_test("I don't know why, but the wheels don't move if this line isn't here.");
-                printf(" ");
+                // TODO: Remove this print statement when we have access to the robot. It may no longer be needed
+                printf(" "); 
                 return ret | this->checkTeleopButtons();
             case RModes::AUTO:
                 return this->checkAutoButtons();
+            // Check for the other modes regardless of whether or not we do anything in them
             case RModes::TEST:
                 return this->checkTestButtons();
             case RModes::DISABLED:
                 return this->checkDisabledButtons();
             default:
+                // This should never happen. But just in case.
                 log_err("Unknown mode %d. Please contact a programmer.",mode);
                 return 1;
         }
@@ -56,10 +63,12 @@ int joystickManager::Update(){
 }
 
 int joystickManager::End(){
+    // Nothing to clean up
     return 0;
 }
 
 bool joystickManager::IsFinished(){
+    // We are never finished. We will work tirelessly until the day we die
     return false;
 }
 
@@ -79,8 +88,8 @@ int joystickManager::checkTeleopButtons(){
         ret |= m_hardware_manager->launch();
     }else if(m_joysticks["buttonBox"]->GetButton(RELEASE_BUTTON)){
         ret |= m_hardware_manager->release();
-    }else if(!m_joysticks["buttonBox"]->GetButton(LAUNCH_INIT_BUTTON)
-            && !m_joysticks["buttonBox"]->GetButton(SUCK_BUTTON)){
+    }else if(!m_joysticks["buttonBox"]->GetButton(LAUNCH_INIT_BUTTON) // We check SUCK here as well, because SUCK utilizes the launch wheels. and we don't want them shutting off before SUCK is done
+            && !m_joysticks["buttonBox"]->GetButton(SUCK_BUTTON)){ // Of course, this creates the side effect that launch-init must be off before those wheels will shut off themselves
         ret |= m_hardware_manager->reset_launch();
     }
 
@@ -106,6 +115,8 @@ int joystickManager::checkTeleopButtons(){
     if(m_joysticks["buttonBox"]->GetButton(WINCH_BUTTON)){
         ret |= m_hardware_manager->init_climb();
     }
+    // Don't need to check for if it isn't being pressed, because it is a one-hit wonder. It can never reset
+
 
     // Do suck stuff
     if(m_joysticks["buttonBox"]->GetButton(SUCK_BUTTON)){
@@ -114,22 +125,30 @@ int joystickManager::checkTeleopButtons(){
         ret |= m_hardware_manager->stop_suck();
     }
 
+    // Finally, if for some bizarre reason any of these return 1, return 1
     return ret;
 }
 
 int joystickManager::checkTestButtons(){
+    // Nothing to check for in Test mode
     return 0;
 }
 
 int joystickManager::checkAutoButtons(){
     int auto_mode = 0;
 
+    // Autonomous mode is decided by a 3-bit binary number, represented by 3 switches on the button-box
+    // The following code converts [B1,B2,B3] into 0's and 1's
+    // Also, we check for AUTO_SWITCH_3 first because it is supposed to be the last digit
+    // 011
+    // ^- This is AUTO_SWITCH_3
     auto_mode |= m_joysticks["buttonBox"]->GetButton(AUTO_SWITCH_3);
     auto_mode <<= 1;
     auto_mode |= m_joysticks["buttonBox"]->GetButton(AUTO_SWITCH_2);
     auto_mode <<= 1;
     auto_mode |= m_joysticks["buttonBox"]->GetButton(AUTO_SWITCH_1);
 
+    // Set the current autonomous manager's autonomous mode to auto mode (only if it isn't already auto_mode, of course)
     if(this->m_robot->getAutoMan()->getMode() != auto_mode)
         this->m_robot->getAutoMan()->setMode(auto_mode);
 
@@ -140,8 +159,10 @@ int joystickManager::checkDisabledButtons(){
     return 0;
 }
 
+// Oh please, I lost my sanity when writing this code during build season long before this method was ever conceived
 int joystickManager::checkSanity(){
     for(auto& set : m_joysticks){
+        // Check each joystick to ensure it isn't NULL, and have them check their own sanity as well.
         if(set.second == NULL) return 1;
         if(!set.second->checkSanity()) return 1;
     }
--- src/joystickManager.h
+++ src/joystickManager.h
@@ -10,26 +10,106 @@
 #include "BaseManager.h"
 #include "HardwareManager.h"
 
-class Robot;
+class Robot; // Forward declaration of Robot (this fixes a compiler-error when we just included it, no idea why though)
 
+// A typedef to make writing this thing easier
 typedef std::map<std::string,joystick*> jmap;
 
 class joystickManager: public BaseManager {
     public:
+        /*
+         * joystickManager - Constructor
+         * Accepts a HardwareManager* and a Robot*
+         * Returns nothing (obviously)
+         * Constructs joystickManager
+         */
         joystickManager(HardwareManager*,Robot*);
+        /*
+         * joystickManager - Destructor
+         * Accepts nothing (obviously)
+         * Returns nothing (obviously)
+         * Deconstructs joystickManager
+         */
         virtual ~joystickManager();
 
+        /*
+         * Init
+         * Accepts nothing
+         * Returns an int (0 upon success, 1 upon failure)
+         * Initializes the joystickManager object and all joystick objects.
+         */
         int Init(void) override;
+
+        /*
+         * Update
+         * Accepts nothing
+         * Returns an int (0 upon success, 1 upon failure)
+         * Updates things related to joysticks
+         */
         int Update(void) override;
+
+        /*
+         * End
+         * Accepts nothing
+         * Returns an int (0 upon success, 1 upon failue)
+         * Cleans up before the destructor is called
+         */
         int End(void) override;
+
+        /*
+         * IsFinished
+         * Accepts nothing
+         * Returns a bool
+         * Returns whether or not the manager is finished
+         */
         bool IsFinished(void) override;
 
+        /*
+         * checkTeleopButtons
+         * Accepts nothing
+         * Returns an int (0 upon success, 1 upon failue)
+         * Checks every button that is supposed to be checked in Teleop mode
+         */
         int checkTeleopButtons();
+
+        /*
+         * checkTestButtons
+         * Accepts nothing
+         * Returns an int (0 upon success, 1 upon failue)
+         * Checks every button that is supposed to be checked in Test mode
+         */
         int checkTestButtons();
+
+        /*
+         * checkAutoButtons
+         * Accepts nothing
+         * Returns an int (0 upon success, 1 upon failue)
+         * Checks every button that is supposed to be checked in Autonomous mode
+         */
         int checkAutoButtons();
+
+        /*
+         * checkDisabledButtons
+         * Accepts nothing
+         * Returns an int (0 upon success, 1 upon failue)
+         * Checks every button that is supposed to be checked in Disabled mode
+         */
         int checkDisabledButtons();
+
+        /*
+         * checkSanity
+         * Accepts nothing
+         * Returns an int
+         * Checks to make sure that everything is working as intended. 0 if it is, 1 otherwise
+         */
         int checkSanity();
 
+        /*
+         * getJoystick
+         * Accepts a string (identifier)
+         * Returns a joystick object
+         * Gets the joystick object specified by str
+         */
         inline joystick* getJoystick(std::string& str){ return m_joysticks[str]; };
 
     private:
